ada177badf7ecb117e98a3ae9862e14ce7cfb3e6
nauvalachid <nauvalachid@gmail.com>
2025-05-08 20:30:39 +0700

    Menambahkan tombol pemilihan tanggal ke TodoPage

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -36,6 +36,25 @@ class TodoPage extends StatelessWidget {
                     ],
                   ),
                   SizedBox(width: 16.0),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        showDatePicker(
+                          context: context, 
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000), 
+                          lastDate: DateTime(2100),
+                        ).then((selectedDate){
+                          if(selectedDate != null) {
+                            context.read<TodoBloc>().add(
+                              TodoSelectDate(date: selectedDate),
+                            );
+                          }
+                        });
+                      },
+                      child: Text('Select Date'),
+                    ),
+                  )
                 ],
               ),
             ],
