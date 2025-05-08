4291f9e44a54b459051d63b7008333e66f48841a
nauvalachid <nauvalachid@gmail.com>
2025-05-08 20:39:43 +0700

    Menambahkan FilledButton untuk mengirimkan item Todo dengan validasi

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -75,7 +75,25 @@ class TodoPage extends StatelessWidget {
                         return null;
                         }
                       ),
-                    )
+                    ),
+                    FilledButton(
+                      onPressed: () {
+                        if (_key.currentState!.validate()) {
+                          final selectedDate = context.read<TodoBloc>().state;
+                          if (selectedDate is TodoLoaded) {
+                            context.read<TodoBloc>().add(
+                              TodoEventAdd(
+                                title: _controller.text, 
+                                date: selectedDate.selectedDate!,
+                                ),
+                              );
+                              _controller.clear();
+                              selectedDate.selectedDate = null;
+                          }
+                        }
+                      }, 
+                      child: Text('Tambah')
+                    ),
                   ],
                 ),
               )
