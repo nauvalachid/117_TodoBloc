0b8a0567425473c21be2a4588d7d45797f0922d4
nauvalachid <nauvalachid@gmail.com>
2025-05-08 20:34:20 +0700

    Menambahkan validator ke kolom teks TodoPage

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -68,6 +68,12 @@ class TodoPage extends StatelessWidget {
                           labelText: 'Todo',
                           border: OutlineInputBorder(),
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a todo';
+                        }
+                        return null;
+                        }
                       ),
                     )
                   ],
