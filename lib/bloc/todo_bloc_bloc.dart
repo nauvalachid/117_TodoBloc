e6b08ff4c3b827f2d13bb88f8a0340f0e2ac2fdb
nauvalachid <nauvalachid@gmail.com>
2025-05-08 19:59:08 +0700

    first commit

---
+++ lib/bloc/todo_bloc_bloc.dart
@@ -0,0 +1,53 @@
+import 'package:bloc/bloc.dart';
+import 'package:latihan2/model/todo.dart';
+
+part 'todo_bloc_event.dart';
+part 'todo_bloc_state.dart';
+
+class TodoBloc extends Bloc<TodoEvent, TodoState> {
+  TodoBloc() : super(TodoLoaded(todos: [], selectedDate: null)) {
+    on<TodoEventAdd>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updateTodos = List.from(currentState.todos);
+        updateTodos.add(
+          Todo(title: event.title, isCompleted: false, date: event.date),
+        );
+        emit(
+          TodoLoaded(
+            todos: updateTodos,
+            selectedDate: currentState.selectedDate,
+          ),
+        );
+      }
+    });
+
+    on<TodoSelectDate>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        emit(TodoLoaded(todos: currentState.todos, selectedDate: event.date));
+      }
+    });
+
+    on<TodoEventComplete>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updateTodos = List.from(currentState.todos);
+        if (event.index >= 0 && event.index < updateTodos.length) {
+          updateTodos[event.index] = Todo(
+            title: updateTodos[event.index].title,
+            isCompleted: updateTodos[event.index].isCompleted == true,
+            //isCompleted: !updateTodos[event.index].isCompleted,
+            date: updateTodos[event.index].date,
+          );
+          emit(
+            TodoLoaded(
+              todos: updateTodos,
+              selectedDate: currentState.selectedDate,
+            ),
+          );
+        }
+      }
+    });
+  }
+}
